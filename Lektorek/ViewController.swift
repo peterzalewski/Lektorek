d9a63f9d87f8bed48e3568ac4f7b7f85af753bff
Peter Zalewski <peter@zalewski.com>
2019-04-01 23:45:08 -0400

    Query search term on button press

--- Lektorek/ViewController.swift
+++ Lektorek/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var searchField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    func getSearchURL(for string: String) -> URL? {
+        // http://lektorek.org/lapi/v2/public/index.php/polish/search/czyta%C4%87?page_size=20&pos=all&diacritics=false
+        var components = URLComponents()
+        components.scheme = "http"
+        components.host = "lektorek.org"
+        components.path = "/lapi/v2/public/index.php/polish/search/\(string)"
+        components.queryItems = [
+            URLQueryItem(name: "page_size", value: "20"),
+            URLQueryItem(name: "pos", value: "all"),
+            URLQueryItem(name: "diacritic", value: "false"),
+        ]
+        return components.url
+    }
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        guard let searchTerm = searchField.text else { return }
+        guard let url = getSearchURL(for: searchTerm) else { return }
+        URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+            if let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary {
+                print(jsonObj)
+            }
+        }).resume()
+    }
 }
-
