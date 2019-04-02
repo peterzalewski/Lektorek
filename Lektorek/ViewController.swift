a8a21343c0b696c954a54534ad64a3f808bf9186
Peter Zalewski <peter@zalewski.com>
2019-04-01 22:32:59 -0400

    Initial commit

---
+++ Lektorek/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lektorek
+//
+//  Created by Pete Zalewski on 4/1/19.
+//  Copyright © 2019 Pete Zalewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
